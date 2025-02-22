b1a8d63b45ce328e68a17de282318c6357c44262
Princy200320 <2024.princy.anthony@ves.ac.in>
2025-02-22 08:07:09 +0530

    Update AppTest.java

--- calculator/src/test/java/com/AppTest.java
+++ calculator/src/test/java/com/AppTest.java
@@ -4,41 +4,16 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+    public AppTest(String testName) {
+        super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-    package com.example.tests;
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class CalculatorTest {
-    @Test
-    public void testAddition() {
-        int result = 1 + 2; // or call your calculator method
-        Assert.assertEquals(result, 3);
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 }
+
 
 
     /**
